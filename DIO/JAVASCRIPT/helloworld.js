65f9a5872f1ab407135dd24abcba5a93d8925a30
relESCampONes <wellington_lima@live.com>
2022-06-25 21:25:41 -0300

    noteorganize

---
+++ DIO/JAVASCRIPT/helloworld.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+
+function vermelho(color){
+    document.body.style.backgroundColor = "red";
+}
+
+function verde(color){
+    document.body.style.backgroundColor = "green";
+}
+
