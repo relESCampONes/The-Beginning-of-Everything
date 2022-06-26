11754940deec60812dade78f8c42cef26d35203a
relESCampONes <wellington_lima@live.com>
2022-06-25 21:58:13 -0300

    notechanged5

---
+++ DIO/while.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+
+
+let k = parseInt(prompt("DIGITE QUANTOS HELLO WORLD QUER VER: "));
+b = 0;
+while (b < k) {
+    console.log("Hello World!!!");
+    b++;
+}
+
